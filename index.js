3ba75804c6957c08e1f057801ca36e072c9bd4e9
Prince Mahmud Piyas <princemahmudpiyas@gmail.com>
2024-12-03 22:57:35 +0600

    first setup

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// 28WCLxU5KTBYSVMM
+// Assignment10
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+var uri =
+  'mongodb://USER:PASS@cluster0-shard-00-00.fk8o9.mongodb.net:27017,cluster0-shard-00-01.fk8o9.mongodb.net:27017,cluster0-shard-00-02.fk8o9.mongodb.net:27017/?ssl=true&replicaSet=atlas-hwpwcj-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db('admin').command({ ping: 1 });
+    const database = client.db('assignment10');
+    const ideaCollection = database.collection('ideas');
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('CrowdCube Server');
+});
+
+app.listen(port, () => {
+  console.log(`CrowdCube server is running on port ${port}`);
+});
