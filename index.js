6f1d17b442abb1b9fe1cec12b394cda0af452bf8
Prince Mahmud Piyas <princemahmudpiyas@gmail.com>
2024-12-04 21:15:50 +0600

    Post added

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// 28WCLxU5KTBYSVMM
-// Assignment10
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-var uri =
-  'mongodb://USER:PASS@cluster0-shard-00-00.fk8o9.mongodb.net:27017,cluster0-shard-00-01.fk8o9.mongodb.net:27017,cluster0-shard-00-02.fk8o9.mongodb.net:27017/?ssl=true&replicaSet=atlas-hwpwcj-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0';
+var uri = `mongodb://${process.env.USER}:${process.env.PASS}@cluster0-shard-00-00.fk8o9.mongodb.net:27017,cluster0-shard-00-01.fk8o9.mongodb.net:27017,cluster0-shard-00-02.fk8o9.mongodb.net:27017/?ssl=true&replicaSet=atlas-hwpwcj-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,7 +27,16 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 });
     const database = client.db('assignment10');
-    const ideaCollection = database.collection('ideas');
+    const campCollection = database.collection('ideas');
+    const userCollection = client.db('assignment10').collection('users');
+
+    app.post('/addCamp', async (req, res) => {
+      const newCamp = req.body;
+      console.log(newCamp);
+      const result = await campCollection.insertOne(newCamp);
+      res.send(result);
+    });
+
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     );
