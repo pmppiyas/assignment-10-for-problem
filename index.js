296d701062326a8b8c64b803f2dafa82089ee26a
Prince Mahmud Piyas <156914923+pmppiyas@users.noreply.github.com>
2024-12-10 22:02:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+
 
     const database = client.db('assignment10');
     const campCollection = database.collection('ideas');
